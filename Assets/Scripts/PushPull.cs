06feb59b42414addf675fb23da42f76ec71a3746
jcr62283 <jericho.raz.584@my.csun.edu>
2023-12-02 21:00:17 -0800

    Improved Push & Pull Functionality - Objects now move toward cursor's position - Fixed "pendulum effect" when holding an object in the air - Adjusted pull effect to be consistent with any framerate

--- Assets/Scripts/PushPull.cs
+++ Assets/Scripts/PushPull.cs
@@ -1,35 +1,67 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PushPull : MonoBehaviour
 {
-    public GameObject affectedObject;
+    Camera mainCamera;
+    GameObject lastAffectedObject;
     public float pushForce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            lastAffectedObject = null;
+        }
+
         if (Input.GetMouseButton(0))
         {
-            Rigidbody2D rigidbody = affectedObject.GetComponent<Rigidbody2D>();
+            moveObject();
+        }
 
-            if (rigidbody != null)
+        if (Input.GetMouseButtonUp(0))
+        {
+            if (lastAffectedObject != null)
             {
-                float mass = rigidbody.mass;
-                rigidbody.AddForce((affectedObject.transform.position - transform.position).normalized * pushForce * mass);
+                lastAffectedObject.GetComponent<Rigidbody2D>().gravityScale = 1.0f;
+            }
+        }
+    }
+
+    void moveObject()
+    {
+        Vector3 cursorPos = mainCamera.ScreenToWorldPoint(Input.mousePosition + (Vector3.forward * 10));
+        Vector3 raycastDirection = Vector3.forward * 10;
+        
+        RaycastHit2D rayHit = Physics2D.Raycast(cursorPos - (Vector3.forward * 10), raycastDirection);
+        
+        if (rayHit.transform != null && rayHit.transform.CompareTag("RayCastDetectable"))
+        {
+            GameObject hitObject = rayHit.transform.gameObject;
+
+            if (hitObject.GetComponent<Rigidbody2D>() != null)
+            {
+                lastAffectedObject = hitObject;
             }
         }
 
-        if (Input.GetMouseButton(1))
+        if (lastAffectedObject != null)
         {
-            Rigidbody2D rigidbody = affectedObject.GetComponent<Rigidbody2D>();
+            Transform affectedTransform = lastAffectedObject.transform;
+            Rigidbody2D affectedRigidbody = lastAffectedObject.GetComponent<Rigidbody2D>();
 
-            if (rigidbody != null)
-            {
-                float mass = rigidbody.mass;
-                rigidbody.AddForce((affectedObject.transform.position - transform.position).normalized * -pushForce * mass);
-            }
+            //affectTransform.position = cursorPos;
+            affectedRigidbody.gravityScale = 0.0f;
+            affectedRigidbody.velocity = Vector3.zero;
+
+            Vector3 towardsCursor = cursorPos - affectedTransform.position;
+            affectedRigidbody.MovePosition(affectedTransform.position + towardsCursor * Time.deltaTime * pushForce);
         }
     }
 }
