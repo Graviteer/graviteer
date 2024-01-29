486c0978dfad1cf87d0e078a500dfe688eb7108a
jcr62283 <jericho.raz.584@my.csun.edu>
2024-01-29 14:27:05 -0800

    Improved Player Movement - Fixed Ground and Ceiling Checks to be more accurate. - Increased Jump Force and Move Speed. - Updated gitignore.

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -6,9 +6,25 @@ public class PlayerMovement : MonoBehaviour
 {
     public CharacterController2D controller;
     public float runSpeed = 40f;
+    public float checkOffset = 0.5f;
     float horizontalMove = 0f;
     bool jump = false;
-    
+
+    void Start()
+    {
+        CapsuleCollider2D collider = GetComponent<CapsuleCollider2D>();
+        float colliderWidth = collider.size.x;
+        float colliderHeight = collider.size.y;
+
+        controller.k_GroundedRadius = colliderWidth;
+        controller.k_CeilingRadius = colliderWidth;
+
+        float ceilingCheckOffset = colliderHeight - (2 * colliderWidth) + checkOffset;
+        float groundCheckOffset = ceilingCheckOffset * -1;
+        controller.m_CeilingCheck.localPosition = new Vector2(collider.offset.x, ceilingCheckOffset);
+        controller.m_GroundCheck.localPosition = new Vector2(collider.offset.x, groundCheckOffset);
+    }
+
     // Update is called once per frame
     void Update()
     {
