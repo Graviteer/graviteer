eb87d1599a06aa47df26bc8aebb84e380f28d741
jcr62283 <jericho.raz.584@my.csun.edu>
2024-03-27 04:57:30 -0700

    Converted Player Movement Controls to New Input System. - Movement and Jumping now use Unity's new Input System. - Updated gitignore.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,15 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class PlayerMovement : MonoBehaviour
 {
+    public InputReader inputReader;
     public CharacterController2D controller;
     public float runSpeed = 40f;
     private Animator m_Animator;
 
     float horizontalMove = 0f;
-    bool jump = false;
+    bool isJumping = false;
 
     BoxCollider2D playerCollider;
 
@@ -18,36 +18,36 @@ public class PlayerMovement : MonoBehaviour
     {
         playerCollider = GetComponent<BoxCollider2D>();
         m_Animator = GetComponent<Animator>();
+
+        inputReader.MoveEvent += HandleMoveInput;
+        inputReader.JumpEvent += HandleJumpInput;
+        inputReader.JumpEndEvent += HandleJumpInputEnd;
     }
 
     // Update is called once per frame
     void Update()
     {
-        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
-        if (Input.GetButton("Jump"))
-        {
-            jump = true;
-        }
-        if (Input.GetButtonUp("Jump"))
-        {
-            jump = false;
-        }
-        if (Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == -1)
-        {
-            m_Animator.SetBool("IsRunning", true);
-        }
-        else
-        {
-            m_Animator.SetBool("IsRunning", false);
-        }
+        m_Animator.SetBool("IsRunning", horizontalMove != 0);
     }
-
 
     void FixedUpdate()
     {
         //Character Movement
-        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, isJumping);
     }
 
+    void HandleMoveInput(float inputValue)
+    {
+        horizontalMove = inputValue * runSpeed;
+    }
 
+    void HandleJumpInput()
+    {
+        isJumping = true;
+    }
+
+    void HandleJumpInputEnd()
+    {
+        isJumping = false;
+    }
 }
