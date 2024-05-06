f03d73a46fcca83944ab0f21649796852b98c828
Damir Kozhamkulov <114121743+Damirchik11@users.noreply.github.com>
2024-05-05 23:43:40 -0700

    Merging main into Damir's Branch

---
+++ Assets/Scripts/loadLevelScripts/NextLevelColliderJ.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class NextLevelColliderJ : MonoBehaviour
+{
+    int sceneBuildIndex { 
+    get { 
+        return SceneManager.GetActiveScene().buildIndex + 1; 
+    }
+}
+    private void OnTriggerEnter2D(Collider2D other) {
+        print("Trigger Entered");
+            // Player entered, so move level
+            print("Switching Scene to " + sceneBuildIndex);
+            SceneManager.LoadScene(sceneBuildIndex, LoadSceneMode.Single);
+        
+    }
+}
